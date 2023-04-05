ffe13933b8f0d1550a6eb6b8b4c99ed6e4268c35
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-05 18:55:56 +0400

    Navbar active link changes on page scrolling

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,29 @@
+// Change navbar accordiong to sections
+let sections = document.querySelectorAll('section');
+let links = document.querySelectorAll('.navigation li a');
+
 // Add a sticky navigation bar
 window.onscroll = () => {
+
+    // Change navbar active link
+    sections.forEach(section => {
+        let top = window.scrollY;
+
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let sectionId = section.getAttribute('id');
+
+        if (top >= offset && top < (offset + height)) {
+
+            links.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.navigation li a[href*=' + sectionId + ']').classList.add('active');
+            });
+
+        };
+    });
+
+    // Add a sticky navigation bar
     const header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
