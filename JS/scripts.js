90eec48065ced57223cc7118f0da7d110e4f8d0f
Daniel Githiomi <danielgithiomi@gmail.com>
2025-11-19 02:18:05 +0400

    Corrected the icon bx-sun spelling

--- JS/scripts.js
+++ JS/scripts.js
@@ -15,14 +15,14 @@ const setTheme = (theme) => localStorage.setItem(THEME_STORAGE_KEY, theme);
 
 const toggleLightMode = () => {
     document.body.classList.remove('dark_mode'),
-    themeToggleIcon.classList.remove('bx-sum'),
+    themeToggleIcon.classList.remove('bx-sun'),
     themeToggleIcon.classList.add('bx-moon')
 }
 
 const toggleDarkMode = () => {
     document.body.classList.add("dark_mode"),
     themeToggleIcon.classList.remove('bx-moon'),
-    themeToggleIcon.classList.add('bx-sum')
+    themeToggleIcon.classList.add('bx-sun')
 }
 
 const loadTheme = () => {
